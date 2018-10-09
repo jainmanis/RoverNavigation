206953e4cae22c7f8f2eea202d7c13666fef014d
Manish Jain <jainmanis@gmail.com>
2018-10-09 11:34:50 +0200

    First commit of empty project and test for rover position

---
+++ PlutoRoverNavigator/RoverTest/TestNavigationController.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using PlutoRoverNavigator.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverTest
+{
+    [TestFixture]
+    public class TestNavigationController
+    {
+        [Test]
+        public void GetPosition_ShouldReturnPosition()
+        {
+            var controller = new NavigationController();
+            string result = controller.GetPosition();
+
+            Assert.AreEqual("Start", result);
+        }
+    }
+}
