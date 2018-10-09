e2559935ae01aa989ea48b2654a0753fe2c75219
Manish Jain <jainmanis@gmail.com>
2018-10-09 15:42:31 +0200

    Added test for testing forward and backward command in all directions

--- PlutoRoverNavigator/PlutoRoverNavigator/Controllers/NavigationController.cs
+++ PlutoRoverNavigator/PlutoRoverNavigator/Controllers/NavigationController.cs
@@ -10,6 +10,7 @@ namespace PlutoRoverNavigator.Controllers
 {
     public class NavigationController : ApiController
     {
+        private List<char> _validCommands = new List<char>() { 'F', 'B', 'L', 'R' };
         public RoverPosition GetPosition()
         {
             return new RoverPosition();
@@ -17,10 +18,53 @@ namespace PlutoRoverNavigator.Controllers
 
         public string ExecuteCommand(char command)
         {
-            if (command == 'F' || command == 'B' || command == 'L' || command == 'R')
+            if (_validCommands.Contains(command))
                 return "command_valid_and_executed";
             else
                 return "invalid_command";
         }
+
+        public RoverPosition ExecuteCommand(char command, RoverPosition position)
+        {
+            if (_validCommands.Contains(command))
+            {
+                if (position.Facing == 'N')
+                {
+                    if (command == 'F')
+                        position.yCoordinate = position.yCoordinate + 1;
+                    else if (command == 'B')
+                        position.yCoordinate = position.yCoordinate - 1;
+                }
+
+                if (position.Facing == 'S')
+                {
+                    if (command == 'F')
+                        position.yCoordinate = position.yCoordinate - 1;
+                    else if (command == 'B')
+                        position.yCoordinate = position.yCoordinate + 1;
+                }
+
+                if (position.Facing == 'E')
+                {
+                    if (command == 'F')
+                        position.xCoordinate = position.xCoordinate + 1;
+                    else if (command == 'B')
+                        position.xCoordinate = position.xCoordinate - 1;
+                }
+
+                if (position.Facing == 'W')
+                {
+                    if (command == 'F')
+                        position.xCoordinate = position.xCoordinate - 1;
+                    else if (command == 'B')
+                        position.xCoordinate = position.xCoordinate + 1;
+                }
+
+                return position;
+            }
+            else
+                return position;
+        }
+
     }
 }
--- PlutoRoverNavigator/RoverTest/TestCommands.cs
+++ PlutoRoverNavigator/RoverTest/TestCommands.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using PlutoRoverNavigator.Controllers;
+using PlutoRoverNavigator.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,79 @@ namespace RoverTest
                 Assert.AreEqual("command_valid_and_executed", output);
             else
                 Assert.AreEqual("invalid_command", output);
+        }
+
+        [Test]
+        public void WhenForwardOrBackwardCommandSend_ReturnNewPosition()
+        {
+            var controller = new NavigationController();
+            RoverPosition currentPosition = new RoverPosition() {
+                xCoordinate = 10,
+                yCoordinate = 10,
+                Facing = 'N'
+            };
+
+            //Forward command facing North
+            RoverPosition newPosition = controller.ExecuteCommand('F', currentPosition);
+
+            Assert.AreEqual(10, newPosition.xCoordinate);
+            Assert.AreEqual(11, newPosition.yCoordinate);
+            Assert.AreEqual('N', newPosition.Facing);
+
+            //Invalid command
+            RoverPosition newPosition2 = controller.ExecuteCommand('X', newPosition);
+
+            Assert.AreEqual(10, newPosition2.xCoordinate);
+            Assert.AreEqual(11, newPosition2.yCoordinate);
+            Assert.AreEqual('N', newPosition2.Facing);
+
+            //Backward command facing north
+            RoverPosition newPosition3 = controller.ExecuteCommand('B', newPosition2);
+
+            Assert.AreEqual(10, newPosition3.xCoordinate);
+            Assert.AreEqual(10, newPosition3.yCoordinate);
+            Assert.AreEqual('N', newPosition3.Facing);
+
+
+            //Forward command facing South
+            currentPosition = new RoverPosition()
+            {
+                xCoordinate = 10,
+                yCoordinate = 10,
+                Facing = 'S'
+            };
+            RoverPosition newPosition4 = controller.ExecuteCommand('F', currentPosition);
+
+            Assert.AreEqual(10, newPosition4.xCoordinate);
+            Assert.AreEqual(9, newPosition4.yCoordinate);
+            Assert.AreEqual('S', newPosition4.Facing);
+
+
+            //Backward command facing West
+            currentPosition = new RoverPosition()
+            {
+                xCoordinate = 10,
+                yCoordinate = 10,
+                Facing = 'W'
+            };
+            RoverPosition newPosition5 = controller.ExecuteCommand('B', currentPosition);
+
+            Assert.AreEqual(11, newPosition5.xCoordinate);
+            Assert.AreEqual(10, newPosition5.yCoordinate);
+            Assert.AreEqual('W', newPosition5.Facing);
+
+            //Forward command facing East
+            currentPosition = new RoverPosition()
+            {
+                xCoordinate = 10,
+                yCoordinate = 10,
+                Facing = 'E'
+            };
+            RoverPosition newPosition6 = controller.ExecuteCommand('F', currentPosition);
+
+            Assert.AreEqual(11, newPosition6.xCoordinate);
+            Assert.AreEqual(10, newPosition6.yCoordinate);
+            Assert.AreEqual('E', newPosition6.Facing);
         }
     }
 }
